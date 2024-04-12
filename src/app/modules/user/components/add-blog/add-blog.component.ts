54bbcde4ce571992bde6f8312ebd7d5bffa58ea9
devamdoshi212 <devamdoshi212@gmail.com>
2024-04-12 21:43:51 +0530

    add blog UI done

--- src/app/modules/user/components/add-blog/add-blog.component.ts
+++ src/app/modules/user/components/add-blog/add-blog.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
-
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-add-blog',
   templateUrl: './add-blog.component.html',
 })
-
 export class AddBlogComponent {
-  toppings = new FormControl('');
+  addBlogForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.addBlogForm = this.fb.group({
+      title: ['', [Validators.required]],
+      content: ['', [Validators.required, Validators.minLength(1)]],
+      image: ['', []],
+      category: [[], [Validators.required]],
+      is_Public: [1, Validators.required],
+    });
+  }
   categories: string[] = [
     'Extra cheese',
     'Mushroom',
@@ -17,4 +32,13 @@ export class AddBlogComponent {
     'Sausage',
     'Tomato',
   ];
+  fileName: string | undefined;
+
+  onFileSelected(event: any): void {
+    const file: File = event.target.files[0];
+    this.fileName = file ? file.name : undefined;
+  }
+  submitForm() {
+    console.log(this.addBlogForm.value);
+  }
 }
