d10b7f2cf0e2ec302b71cad64e4af28e492c9fa5
devamdoshi212 <devamdoshi212@gmail.com>
2024-04-13 00:49:06 +0530

    Personal Blog Done

--- src/app/modules/user/components/blog-card/blog-card.component.ts
+++ src/app/modules/user/components/blog-card/blog-card.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-blog-card',
   templateUrl: './blog-card.component.html',
 })
-export class BlogCardComponent {}
+export class BlogCardComponent {
+  @Input() blog: any;
+  @Input() personal: any = true;
+}
--- src/app/modules/user/components/personal-blogs/personal-blogs.component.ts
+++ src/app/modules/user/components/personal-blogs/personal-blogs.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../../../services/user.service';
 
 @Component({
   selector: 'app-personal-blogs',
   templateUrl: './personal-blogs.component.html',
 })
-export class PersonalBlogsComponent {}
+export class PersonalBlogsComponent {
+  blogs: any[] = [];
+  constructor(private user: UserService) {
+    this.user.getPersonalBlog().subscribe((data) => {
+      if (data.success) {
+        this.blogs = data.data.blogs;
+      }
+    });
+  }
+}
